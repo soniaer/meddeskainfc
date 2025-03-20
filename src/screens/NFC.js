cf12a17aaf1eb4fc639a4b2d4f604bfb2720450c
soniaer <soniaer987@gmail.com>
2025-03-20 09:22:10 +0530

    Update NFC.js

--- src/screens/NFC.js
+++ src/screens/NFC.js
@@ -21,6 +21,58 @@ const [Additional_Data,setAdditional_Data] = useState("")
 
 const [message, setMessage] = useState('');
 
+const scan = async() => {
+    if ("NDEFReader" in window) {
+        try {
+          console.log("in ndef window")
+            const ndef = new window.NDEFReader();
+            await ndef.scan();
+
+            console.log("Scan started successfully.");
+            ndef.onreadingerror = () => {
+                alert("Cannot read data from the NFC tag. Try another one?");
+            };
+
+            ndef.onreading = (event) => {
+                console.log("NDEF message read.");
+                onReading(event); //Find function below
+            };
+        } catch (error) {
+            console.log(`Error! Scan failed to start: ${error}.`);
+        }
+    }else{
+      console.log("not in  ndef window")
+
+    }
+};
+
+
+const onReading = ({message, serialNumber}) => {
+  console.log(serialNumber);
+  for (const record of message.records) {
+      switch (record.recordType) {
+          case "text":
+              const textDecoder = new TextDecoder(record.encoding);
+              console.log("Message", textDecoder.decode(record.data));
+              setMessage(textDecoder.decode(record.data))
+              break;
+          case "url":
+                        setMessage(textDecoder.decode(record.data))
+
+              // TODO: Read URL record with record data.
+              break;
+          default:
+                        setMessage(textDecoder.decode(record.data))
+
+              // TODO: Handle other records with record data.
+      }
+  }
+};
+
+useEffect(()=>{
+scan()
+},[scan])
+
 const navigate = useNavigate();
 
 const adddata =async() =>{
@@ -187,9 +239,8 @@ console.log(file?.name,"Product_iamge")
                 // Read the file as a Data URL (Base64)
                 reader.readAsDataURL(file);
   }
-  fileInput.value = '';
+fileInput.value = '';
 }
-
 
 return (
 <div id="main"
@@ -201,12 +252,9 @@ height: "137vh",
 alignItems:"center",
 justifyContent:"center",
 fontFamily:"Poppins",
-
 }}
 >
 <div style={{height:"100%", width: "100%",overflowX:"hidden" }}>
-
-
 <div style={{ width: "94%",
 maxHeight:'100px',height:"100%",marginLeft:"3%",
 backgroundColor:"transparent",
@@ -216,12 +264,8 @@ alignItems:"flex-start",
 color:"#fff",fontSize:"28px",paddingTop:"2%"
 }}>
 <div style={{display:"flex",width:"100%",justifyContent:"space-between", }}>
-
-
 <div style={{width:"60%",backgroundColor:"#fff",justifyContent:"space-between",
 display:"flex",padding:"1%",maxHeight:"70px",}}>
-
-
 <img id="logodiv" alt="" src={require('../img/med.png')} style={{width:"95%",height:"26px"}} />
 </div>
 <div style={{ width: "55%",border:"3px solid #9ea3ac", 
@@ -277,7 +321,6 @@ justifyContent:"flex-start",textAlign:"center",
 alignItems:"center",color:"#000",fontSize:"13px",display:"flex",fontWeight:"600",paddingLeft:"3%",
 }}>
 FIRST NAME:<span style={{color:"#000",fontSize:"13px",fontWeight:"500",marginLeft:"5%",overflow:"hidden",}}>{scannedData?.First_Name}</span>
-
 </div>
 <div style={{ width: "95%",backgroundColor:"#f3f4f6",border:"3px solid #9ea3ac", 
 maxHeight:'80px',height:"100%",
@@ -285,7 +328,6 @@ justifyContent:"flex-start",textAlign:"center",
 alignItems:"center",color:"#000",fontSize:"13px",display:"flex",fontWeight:"600",paddingLeft:"3%",
 }}>
 LAST NAME:<span style={{color:"#000",fontSize:"13px",fontWeight:"500",marginLeft:"5%",overflow:"hidden",}}>{scannedData?.Last_Name}</span>
-
 </div>
 <div style={{ width: "95%",backgroundColor:"#f3f4f6",border:"3px solid #9ea3ac", 
 maxHeight:'80px',height:"100%",
@@ -293,7 +335,6 @@ justifyContent:"flex-start",textAlign:"center",
 alignItems:"center",color:"#000",fontSize:"13px",display:"flex",fontWeight:"600",paddingLeft:"3%",
 }}>
 DATE OF BIRTH:<span style={{color:"#000",fontSize:"13px",fontWeight:"500",marginLeft:"5%",overflow:"hidden",}}>{scannedData?.Date_Of_Birth}</span>
-
 </div>
 <div style={{ width: "95%",backgroundColor:"#f3f4f6",border:"3px solid #9ea3ac", 
 maxHeight:'80px',height:"100%",
@@ -309,16 +350,13 @@ justifyContent:"flex-start",textAlign:"center",
 alignItems:"center",color:"#000",fontSize:"13px",display:"flex",fontWeight:"600",paddingLeft:"3%",
 }}>
 PATIENT AGE:<span style={{color:"#000",fontSize:"13px",fontWeight:"500",marginLeft:"5%",overflow:"hidden",}}>{scannedData?.Age}</span>
-
 </div>
-
 <div style={{ width: "95%",backgroundColor:"#f3f4f6",border:"3px solid #9ea3ac", 
 maxHeight:'80px',height:"100%",
 justifyContent:"flex-start",textAlign:"center",
 alignItems:"center",color:"#000",fontSize:"13px",display:"flex",fontWeight:"600",paddingLeft:"3%",
 }}>
 PATIENT HEIGHT:<span style={{color:"#000",fontSize:"13px",fontWeight:"500",marginLeft:"5%",overflow:"hidden",}}>{scannedData?.Height}</span>
-
 </div>
 <div style={{ width: "95%",backgroundColor:"#f3f4f6",border:"3px solid #9ea3ac", 
 maxHeight:'80px',height:"100%",
@@ -326,24 +364,20 @@ justifyContent:"flex-start",textAlign:"center",
 alignItems:"center",color:"#000",fontSize:"13px",display:"flex",fontWeight:"600",paddingLeft:"3%",
 }}>
 PATIENT WEIGHT:<span style={{color:"#000",fontSize:"13px",fontWeight:"500",marginLeft:"5%",overflow:"hidden",}}>{scannedData?.Weight}</span>
-
 </div>
 <div style={{ width: "95%",backgroundColor:"#f3f4f6",border:"3px solid #9ea3ac", 
 maxHeight:'80px',height:"100%",
 justifyContent:"flex-start",textAlign:"center",
 alignItems:"center",color:"#000",fontSize:"13px",display:"flex",fontWeight:"600",paddingLeft:"3%",
 }}>
-
 ADDRESS:<span style={{color:"#000",fontSize:"13px",fontWeight:"500",marginLeft:"5%",overflow:"hidden",}}>{scannedData?.Address}</span>
 </div>
 <div style={{ width: "95%",backgroundColor:"#f3f4f6",border:"3px solid #9ea3ac", 
 maxHeight:'80px',height:"100%",
 justifyContent:"flex-start",textAlign:"center",
 alignItems:"center",color:"#000",fontSize:"13px",display:"flex",fontWeight:"600",paddingLeft:"3%",
-
 }}>
 PHONE NUMBER:<span style={{color:"#000",fontSize:"13px",fontWeight:"500",marginLeft:"5%",overflow:"hidden",}}>{scannedData?.Phone_Number}</span>
-
 </div>
 <div style={{ width: "95%",backgroundColor:"#f3f4f6",border:"3px solid #9ea3ac", 
 maxHeight:'80px',height:"100%",
@@ -351,7 +385,6 @@ justifyContent:"flex-start",textAlign:"center",
 alignItems:"center",color:"#000",fontSize:"13px",display:"flex",fontWeight:"600",paddingLeft:"3%",
 }}>
 PRIMARY PHYSICIAN:<span style={{color:"#000",fontSize:"13px",fontWeight:"500",marginLeft:"5%",overflow:"hidden",}}>{scannedData?.Primary_Physician}</span>
-
 </div>
 </div>
 <div style={{ width: "72%",
@@ -361,7 +394,6 @@ display:"flex",color:"#000",fontSize:"18px",
 backgroundColor:"#fff",
 flexDirection:"column",alignItems:"center"
 }}>
-
 <p style={{textAlign:"right",width:"99%",color:"#9ea3ac",fontFamily:"Poppins",paddingRight:"2%"}}>Data</p>
 <div style={{ width: "98%",border:"2px solid #d1d5db",maxHeight:"380px",
 height:"100%",marginTop:"3%"}}  class="table table-responsive" >
@@ -370,7 +402,6 @@ height:"100%",marginTop:"3%"}}  class="table table-responsive" >
 <tr style={{backgroundColor:"#e5e7eb"}}>
 <th style={{borderRight:"1px solid #d1d5db"}}>DATE of visit</th>
 <th style={{border:"none"}}>Addititonal Data</th>
-
 </tr>
 </thead>
 <tbody  style={{fontWeight:"500",fontSize:"14px",textAlign:"left",
@@ -378,52 +409,42 @@ backgroundColor:"#d1d5db"}}>
 <tr>
 <td style={{border:"none"}}>{scannedData?.Date_Of_Visit}</td>
 <td style={{border:"none"}}>{scannedData?.Additional_Data}</td>
-
 </tr>
 <tr>
 <td style={{border:"none"}}>{scannedData?.Date_Of_Visit}</td>
 <td style={{border:"none"}}>{scannedData?.Additional_Data}</td>
-
 </tr>
 <tr>
 <td style={{backgroundColor:"#fff",borderBottom:"2px solid #d1d5db"}}>{scannedData?.Date_Of_Visit}</td>
 <td style={{borderRight:"1px solid #000",borderTop:"none"}}>{scannedData?.Additional_Data}</td>
-
 </tr>
 <tr>
 <td style={{backgroundColor:"#fff",borderBottom:"2px solid #d1d5db"}}>{scannedData?.Date_Of_Visit}</td>
 <td style={{borderTop:"1px solid #000",borderRight:"1px solid #000"}}>{scannedData?.Additional_Data}</td>
-
 </tr>
 <tr>
 <td style={{backgroundColor:"#fff",borderBottom:"2px solid #d1d5db"}}>{scannedData?.Date_Of_Visit}</td>
 <td style={{borderTop:"1px solid #000",borderRight:"1px solid #000"}}>{scannedData?.Additional_Data}</td>
-
 </tr>
 <tr>
 <td style={{backgroundColor:"#fff",borderBottom:"2px solid #d1d5db"}}>{scannedData?.Date_Of_Visit}</td>
 <td style={{borderTop:"1px solid #000",borderRight:"1px solid #000"}}>{scannedData?.Additional_Data}</td>
-
 </tr>
 <tr>
 <td style={{backgroundColor:"#fff",borderBottom:"2px solid #d1d5db"}}>{scannedData?.Date_Of_Visit}</td>
 <td style={{borderTop:"1px solid #000",borderRight:"1px solid #000"}}>{scannedData?.Additional_Data}</td>
-
 </tr>
 <tr>
 <td style={{backgroundColor:"#fff",border:"none"}}>{scannedData?.Date_Of_Visit}</td>
 <td style={{borderTop:"1px solid #000",borderRight:"1px solid #000",
 borderBottom:"1px solid #000"}}>{scannedData?.Additional_Data}</td>
-
 </tr>
 </tbody>
 </table>
 </div>
 </div>
 </div>
-
 </div>
-
 <div style={{ width: "54%",
 maxHeight:'419px',height:"100%",marginLeft:"0%",marginTop:"0%",
 textAlign:"center",
@@ -433,7 +454,6 @@ alignItems:"center",display:"flex",color:"#fff",fontSize:"18px", flexDirection:"
 maxHeight:'300px',height:"100%",marginLeft:"0%",marginTop:"0%",
 justifyContent:"center",textAlign:"center",
 alignItems:"center",display:"flex",color:"#fff",fontSize:"18px",flexDirection:"column"
-
 }}>Picture
 <img alt="" src={scannedData?.Image} style={{width:"50%",marginTop:"5%"}} />   </div>
 <div  style={{border:"2px solid #fff", width: "74%",
@@ -441,17 +461,11 @@ maxHeight:'40px',height:"100%",marginLeft:"0%",marginTop:"8%",
 justifyContent:"center",textAlign:"center",overflow:"hidden",
 alignItems:"center",display:"flex",color:"#fff",fontSize:"18px"
 }}>{scannedData?.Patient_Id}</div>
-
 </div>
-
 </div>
 <div>
-
 <div style={{marginTop:"19.55vh",width:"96%",paddingLeft:"2%",color:"white",paddingRight:"0%"}}>
-
 <div style={{fontSize:"16px"}}>
-
-
 Disclaimers and warning :
 </div>
 <div style={{fontSize:"16px",fontWeight:"600"}}>
@@ -464,66 +478,51 @@ be construed as real data or representative of any actual real medical data .
 This webpage is been powered by MedDesk-AI all rights received ©
 </div>
 <div id="myModal" class="modal">
-
 <div class="modal-content">
 <span class="close" onClick={()=>{document.getElementById("myModal").style.display="none"}}>&times;</span>
-
-
-
 <h4 style={{color:"#156082"}}>Add Data</h4>
 <div style={{display:"flex",justifyContent:"center",alignItems:"center",width:"100%"}}>
-
-
 <div style={{width:"80%",paddingTop:"1%",paddingBottom:"1%",color:"#156082",display:"flex",justifyContent:"space-between",
 flexDirection:"column"}}>
 <div style={{width:"100%",paddingTop:"1%",paddingBottom:"1%",color:"#156082",display:"flex",justifyContent:"space-between",
 }}>
-
-
 <label>PATIENT NAME:</label>
-
 <input value={Patient_Name} onChange={(e)=>setPatient_Name(e.target.value)} style={{width:"60%",border:"2px solid #156082",marginBottom:"2%"}}>
 </input>
 </div>
 <div style={{width:"100%",paddingTop:"1%",paddingBottom:"1%",color:"#156082",display:"flex",justifyContent:"space-between",
 }}>
 <label>FIRST NAME:</label>
-
 <input value={First_Name} onChange={(e)=>setFirst_Name(e.target.value)} style={{width:"60%",border:"2px solid #156082",marginBottom:"2%"}}>
 </input>
 </div>
 <div style={{width:"100%",paddingTop:"1%",paddingBottom:"1%",color:"#156082",display:"flex",justifyContent:"space-between",
 }}>
 <label>LAST NAME:</label>
-
 <input value={Last_Name} onChange={(e)=>setLast_Name(e.target.value)} style={{width:"60%",border:"2px solid #156082",marginBottom:"2%"}}>
 </input>
 </div>
 <div style={{width:"100%",paddingTop:"1%",paddingBottom:"1%",color:"#156082",display:"flex",justifyContent:"space-between",
 }}>
 <label>DATE OF BIRTH:</label>
-
 <input value={Date_Of_Birth} onChange={(e)=>setDate_Of_Birth(e.target.value)} style={{width:"60%",border:"2px solid #156082",marginBottom:"2%"}}>
 </input>
 </div>
 <div style={{width:"100%",paddingTop:"1%",paddingBottom:"1%",color:"#156082",display:"flex",justifyContent:"space-between",
 }}>
 <label>PATIENT ID:</label>
-
 <input value={Patient_Id} onChange={(e)=>setPatient_Id(e.target.value)} style={{width:"60%",border:"2px solid #156082",marginBottom:"2%"}}>
 </input>
 </div>
 <div style={{width:"100%",paddingTop:"1%",paddingBottom:"1%",color:"#156082",display:"flex",justifyContent:"space-between",
 }}>
 <label>PATIENT AGE:</label>
-
 <input value={Age} onChange={(e)=>setAge(e.target.value)} style={{width:"60%",border:"2px solid #156082",marginBottom:"2%"}}>
 </input>
 </div>
 <div style={{width:"100%",paddingTop:"1%",paddingBottom:"1%",color:"#156082",display:"flex",justifyContent:"space-between",
 }}>
 <label>PATIENT IMAGE:</label>
-
 <input type="file" id="file-input" onChange={(e)=>{sendMessage()}} style={{width:"20%",border:"2px solid #156082",marginBottom:"2%"}}>
 </input>
 <div id="fileName" style={{marginTop:"0.5%",overflow:"hidden",width:"25%",maxHeight:"20px"}}>No file chosen</div>
@@ -531,28 +530,24 @@ flexDirection:"column"}}>
 <div style={{width:"100%",paddingTop:"1%",paddingBottom:"1%",color:"#156082",display:"flex",justifyContent:"space-between",
 }}>
 <label>PATIENT HEIGHT:</label>
-
 <input value={Height} onChange={(e)=>setHeight(e.target.value)} style={{width:"60%",border:"2px solid #156082",marginBottom:"2%"}}>
 </input>
 </div>
 <div style={{width:"100%",paddingTop:"1%",paddingBottom:"1%",color:"#156082",display:"flex",justifyContent:"space-between",
 }}>
 <label>PATIENT WEIGHT:</label>
-
 <input value={Weight} onChange={(e)=>setWeight(e.target.value)} style={{width:"60%",border:"2px solid #156082",marginBottom:"2%"}}>
 </input>
 </div>
 <div style={{width:"100%",paddingTop:"1%",paddingBottom:"1%",color:"#156082",display:"flex",justifyContent:"space-between",
 }}>
 <label>ADDRESS:</label>
-
 <input value={Address} onChange={(e)=>setAddress(e.target.value)} style={{width:"60%",border:"2px solid #156082",marginBottom:"2%"}}>
 </input>
 </div>
 <div  style={{width:"100%",paddingTop:"1%",paddingBottom:"1%",color:"#156082",display:"flex",justifyContent:"space-between",
 }}>
 <label>PHONE NUMBER:</label>
-
 <input value={Phone_Number} onChange={(e)=>setPhone_Number(e.target.value)} style={{width:"60%",border:"2px solid #156082",marginBottom:"2%"}}>
 </input>
 </div>
@@ -566,14 +561,12 @@ flexDirection:"column"}}>
 <div style={{width:"100%",paddingTop:"1%",paddingBottom:"1%",color:"#156082",display:"flex",justifyContent:"space-between",
 }}>
 <label>DATE OF VISIT:</label>
-
 <input value={Date_Of_Visit} onChange={(e)=>setDate_Of_Visit(e.target.value)} style={{width:"60%",border:"2px solid #156082",marginBottom:"2%"}}>
 </input>
 </div>
 <div  style={{width:"100%",paddingTop:"1%",paddingBottom:"1%",color:"#156082",display:"flex",justifyContent:"space-between",
 }}>
 <label>ADDITIONAL DATA:</label>
-
 <input value={Additional_Data} onChange={(e)=>setAdditional_Data(e.target.value)} style={{width:"60%",border:"2px solid #156082",marginBottom:"2%"}}>
 </input>
 </div>
@@ -582,8 +575,6 @@ flexDirection:"column"}}>
 <button onClick={()=>{adddata();document.getElementById("myModal").style.display="none"}} 
 style={{border:"2px solid #156082",width:"40%",backgroundColor:"#156082",height:"30px",
 cursor:"pointer"}}>
-
-
 <span style={{color:"white"}} >
 Add</span>
 </button>
@@ -592,17 +583,13 @@ cursor:"pointer",color:"white"
 }}>View</button>
 </div>
 </div>
-
-</div>
-
 </div>
 </div>
 </div>
 </div>
 </div>
 </div>
-
-
+</div>
 );
 }
 
